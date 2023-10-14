86d047aac85ae7a813c48e3f75894ff4620b568b
Abhikappana <abhikappana@gmail.com>
2023-10-14 10:20:10 +0530

    commit

--- 26.js
+++ 26.js
@@ -1,6 +1,12 @@
 const prompt = require('prompt-sync')()
-let input =prompt("Enter a input")
+let input =prompt("Enter a input : ")
 try{
-    let reverse=input.split().reverse().join(); 
-    console.log("Reversed String is :")
+    let reverse=input.split("").reverse().join(); 
+    console.log("Reversed String is : "+reverse);
+}
+catch(err){
+    console.log("Error : "+err.message);
+}
+finally{
+    console.log(`Type of ${input} is ${typeof(input)}`);
 }
